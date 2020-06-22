ae4f6b842a1313f99b73d155cdaeeb5512900511
LokenSheng <1252431372@qq.com>
2020-06-22 21:02:42 +0800

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        num1: 0,
-        num2: 0,
-        type: '+',
-        result: 0
+        questionList: [
+            {
+                type: 'short',
+                title: '1.请问你的姓名是？',
+                chooseList: null,
+                id: 0,
+            },
+            {
+                type: 'single',
+                title: '2.请问您的性别是？',
+                chooseList: [
+                    '男',
+                    '女',
+                    '保密',
+                ],
+                id: 1,
+            },
+            {
+                type: 'multiple',
+                title: '3. 请选择您的兴趣爱好：',
+                chooseList: [
+                    '看书',
+                    '游泳',
+                    '跑步',
+                    '看电影',
+                    '听音乐',
+                ],
+                id: 2,
+            },
+            {
+                type: 'long',
+                title: '4. 请介绍一下自己:',
+                chooseList: null,
+                id: 3,
+            },
+        ]
     },
     methods: {
-        handleClick() {
-            const { type, num1, num2 } = this;
-            switch (type) {
-                case '+':
-                    this.result = num1 + num2;
-                    break;
-                case '-':
-                    this.result = num1 - num2;
-                    break;
-                case '*':
-                    this.result = num1 * num2;
-                    break;
-                case '/':
-                    this.result = num1 / num2;
-                    break;
-            }
-        }
+
     }
 })
+
+vm.questionList.forEach(question => {
+    const answer = question.type === 'multiple' ? [] : '';
+    vm.$set(question, 'answer', answer);
+  });
